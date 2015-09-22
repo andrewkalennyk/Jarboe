8ee6b2dbf7c87494e6a2af2cce45e3399be1d227
Oles Kashchenko <oleskashchenko@gmail.com>
2015-09-22 10:41:40 +0300

    ckeditor bug fix

--- src/views/tb/input_wysiwyg_ckeditor.blade.php
+++ src/views/tb/input_wysiwyg_ckeditor.blade.php
@@ -1,12 +1,10 @@
 <textarea id="{{$name}}-wysiwyg" name="{{ $name }}" class="ckeditor">{{ $value }}</textarea>
 <script type="text/javascript">
     jQuery(document).ready(function() {
-        CKEDITOR.replace('{{$name}}-wysiwyg');
-
-        CKEDITOR.on('instanceCreated', function(e) {
+        CKEDITOR.on('instanceCreated', function (e) {
             if (e.editor.name === '{{$name}}-wysiwyg') {
-                e.editor.on('change', function(event) {
-                    jQuery('textarea#{{$name}}-wysiwyg').val(e.editor.getData());
+                e.editor.on('change', function (event) {
+                    jQuery('textarea#' + e.editor.name).val(e.editor.getData());
                 });
             }
         });
@@ -28,5 +26,7 @@
 
         CKEDITOR.config.removeButtons = '{{$editorButtons}}';
         CKEDITOR.config.extraPlugins = "ImageManager";
+
+        CKEDITOR.replace('{{$name}}-wysiwyg');
     });
 </script>

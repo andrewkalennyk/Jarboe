fd798167d1ea8ef313aed19a13db6df95031b23e
Oles Kashchenko <oleskashchenko@gmail.com>
2015-10-20 15:35:24 +0200

    test submodule

--- src/config/admin.php
+++ src/config/admin.php
@@ -1,20 +1,16 @@
 <?php
 
 return array(
-
     'caption'  => 'Admin caption',
     'logo_url' => asset('packages/yaro/jarboe/img/logo.png'),
     'favicon_url' => asset('packages/yaro/jarboe/img/favicon/favicon.ico'),
-    
     'uri' => '/admin',
-
     'user_name'  => function() {
         return Sentry::getUser()->first_name .' '. Sentry::getUser()->last_name;
     },
     'user_image' => function() {
         return 'https://www.gravatar.com/avatar/'. md5(Sentry::getUser()->email);
     },
-
     'menu' => array(
         array(
             'title' => 'Главная',
